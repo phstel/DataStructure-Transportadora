d4dd4e83aeafbd78a92b2f6e461ace6b4cf82f4f
Pedro Stelzer <pedro.stelzer@protonmail.com>
2025-04-15 22:34:03 -0300

    inclusao da animacao e do settimeout

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let pacotesEntregues = [];
 const filaPacotes = document.getElementById('filaPacotes');
 const entregasRealizadas = document.getElementById('entregasPacote');
 const informacoesEntrega = document.getElementById('infoEntrega');
+const entregador = document.getElementById('entregador');
 
 async function gerarDestinatario() {
     const resposta = await fetch('https://randomuser.me/api/');
@@ -11,8 +12,13 @@ async function gerarDestinatario() {
     return dados.results[0];
 }
 
+function gerarTempoEntrega() {
+    return Math.floor(Math.random() * 5);
+};
+
 async function gerarPacote() {
     const destinatario = await gerarDestinatario();
+    const tempoEntrega = gerarTempoEntrega();
     
     return {
         id: Date.now(),
@@ -20,6 +26,7 @@ async function gerarPacote() {
         endereco: `${destinatario.location.street.name}, ${destinatario.location.street.number}`,
         cidade: `${destinatario.location.city}, ${destinatario.location.state}`,
         pais: destinatario.location.country,
+        tempoEntrega: tempoEntrega,
         foto: destinatario.picture.thumbnail
     }
 }
@@ -46,6 +53,7 @@ function atualizarFilaDePacotes() {
             <p><strong>Endereço:</strong> ${pacote.endereco}</p>
             <p><strong>Cidade:</strong> ${pacote.cidade}</p>
             <p><strong>País:</strong> ${pacote.pais}</p>
+            <p class="tempo"><strong>Tempo estimado:</strong> ${pacote.tempoEntrega} segundos</p>
         `;
         filaPacotes.appendChild(pacoteElement);
     });
@@ -56,15 +64,23 @@ function entregarPacote() {
     informacoesEntrega.innerHTML = `
         <h3>Entregando pacote para ${pacote.destinatario}</h3>
         <p>Endereço: ${pacote.endereco}, ${pacote.cidade}</p>
+        <p>Tempo estimado: ${pacote.tempoEntrega} segundos</p>
     `;
-    
-    const pacoteEntregue = filaDePacotes.shift();
+   
+    setTimeout(() => {
+        entregador.style.left = 'calc(100% - 90px)';
 
-    pacotesEntregues.unshift(pacoteEntregue);
+        setTimeout(() => {
+            const pacoteEntregue = filaDePacotes.shift();
+            pacotesEntregues.unshift(pacoteEntregue);
 
-    atualizarFilaDePacotes();
-    atualizarEntregasRealizadas();
+            atualizarFilaDePacotes();
+            atualizarEntregasRealizadas();
 
+            entregador.style.left = '10px';
+            informacoesEntrega.innerHTML = '<h3>Entrega concluída!</h3>';
+        }, pacote.tempoEntrega * 1000);
+    }, 100);
 }
 
 function atualizarEntregasRealizadas() {
@@ -82,6 +98,7 @@ function atualizarEntregasRealizadas() {
             <p><strong>Endereço:</strong> ${pacote.endereco}</p>
             <p><strong>Cidade:</strong> ${pacote.cidade}</p>
             <p><strong>País:</strong> ${pacote.pais}</p>
+            <p class="tempo"><strong>Tempo de entrega:</strong> ${pacote.tempoEntrega} segundos</p>
         `;
         entregasRealizadas.appendChild(pacoteElement);
     });
